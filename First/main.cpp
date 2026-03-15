4c33277c5cba66e4d59d05f804bc3cef39e0a9e2
AMANTRIPATHI12 <amantripathi2244@gmail.com>
2026-03-15 20:00:30 +0530

    test

---
+++ First/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // one of the files of standard librarey of c++ for input output.
+
+int main() { // this is the entry point of the program and its int type tells what this function will return.
+    // always end statements with a semi colon;
+    std::cout << "Hello multiverse";  // this line is known as statement because it tells the os what to do.
+    return 0; // Here zero tells the os that the program terminates correctly if we return any value other than 0 than it means our program encountered an error.
+}
